ce891d6f267303a334a92b62b56693dd8da8d838
NikosGour <nikosgournak@gmail.com>
2025-12-23 16:35:18 +0200

    add primevue

--- src/components/server/Main.vue
+++ src/components/server/Main.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import  Button from 'primevue/button';
+
 </script>
 <template>
-	<div class="bg-blue-400 w-full"></div>
+	<div class="bg-blue-400 w-full">
+		<Button > NIkos</Button>
+	</div>
+
 </template>
 
 <style scoped></style>
--- src/components/server/User.vue
+++ src/components/server/User.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type User from '@/types/user';
+import { Chip } from 'primevue';
 
 type Props = {
 	user: User
@@ -8,5 +9,9 @@ const props = defineProps<Props>();
 
 </script>
 <template>
-	<div class="bg-red-400 rounded border shadow mb-1.5 p-1 truncate">{{ props.user.username }}</div>
+	<Chip class="w-full mb-1.5">
+		<span class="truncate">{{ props.user.username }}</span>
+		<span class="text-white bg-green-700 rounded-full w-14 h-8 flex items-center justify-center">P</span>
+	</Chip>
+	<!-- <div class="bg-red-400 rounded border shadow mb-1.5 p-1 truncate">{{ props.user.username }}</div> -->
 </template>
