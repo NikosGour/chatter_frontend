b1d9a2d082edac5c90a3e005cede3a996434c20c
NikosGour <nikosgournak@gmail.com>
2026-01-02 22:18:15 +0200

    add timeout for server page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import ServerPage from './components/ServerPage.vue';
+import type Server from './types/server.js';
+import { GetServerById } from './services/server.js';
+import { sleep } from './utils/sleep.js';
 
+const server = ref<Server | null>(null);
+const server_id = `6d17edd9-f15f-4001-83aa-9b2c6ce6ba9b`;
+const fetchServer = async() => {
+	await sleep(10 * 1000);
+	const ser = await GetServerById(server_id);
+	clearTimeout(timeout_id);
+	console.log(`Fetched users:`, ser);
+	server.value = ser;
+};
+
+fetchServer();
+
+const hit_timeout = ref<boolean>(false);
+const timeout = 5 * 1000;
+const timeout_id = setTimeout(() => {
+	hit_timeout.value = true;
+}, timeout);
 </script>
 
 <template>
-  <ServerPage server_id="6d17edd9-f15f-4001-83aa-9b2c6ce6ba9b" />
+  <ServerPage v-if="server" :server="server"/>
+  <div v-else-if="!hit_timeout" class="flex justify-center items-center w-screen h-screen">
+	<span class="pi pi-spin pi-spinner" style="font-size: 3em;"></span>
+  </div>
+  <div v-else class="flex justify-center items-center w-screen h-screen bg-red-700">
+	<span class="text-3xl font-bold text-white">
+		<pre>
+Could not connect to the server.
+Please check your internet connection.
+		</pre>
+	</span>
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/ServerPage.vue
+++ src/components/ServerPage.vue
@@ -9,30 +9,22 @@ import type Tab_t from '@/types/tab';
 import { Injectables } from '@/utils/injectables';
 
 type Props = {
-	server_id: string
+	server: Server
 };
 const props = defineProps<Props>();
 
-const server = ref<Server | null>(null);
 
 const active_tab = ref<Tab_t | null>(null);
 
 provide(Injectables.active_tab, active_tab);
 
-const fetchServer = async() => {
-	const ser = await GetServerById(props.server_id);
-	console.log(`Fetched users:`, ser);
-	server.value = ser;
-};
-
-fetchServer();
 </script>
 
 <template>
 	<div class="grid grid-cols-[20%_60%_20%] h-screen">
-		<UserList :server_id="props.server_id" />
+		<UserList :server_id="props.server.id" />
 		<Main v-if="server" :server="server"/>
-		<TabList  :server_id="props.server_id" />
+		<TabList  :server_id="props.server.id" />
 	</div>
 </template>
 
